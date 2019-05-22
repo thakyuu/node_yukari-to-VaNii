f22dceea966261c4e41badf02e160b91e8819114
thakyuu <thakyuu@users.noreply.github.com>
2019-05-22 19:39:52 +0900

    ・書き込むページ数の指定に対応 ・プロセスの終了処理を修正

--- node_yukari-to-VaNii.js
+++ node_yukari-to-VaNii.js
@@ -3,27 +3,34 @@
 //Library
 const fs = require('fs');
 const iconvLite = require('iconv-lite');
-const osc = require('node-osc')
+const osc = require('node-osc');
+
+process.on('uncaughtException', err => {
+	console.log(err);
+	process.exit(1);
+})
 
 //Static
 const config = JSON.parse(fs.readFileSync(__dirname + '/config.json').toString());
 const message = process.argv.slice(2).join(' ');
+const template = JSON.parse(fs.readFileSync(config.template.path).toString());
+const osc_client = new osc.Client(config.osc.targetAddress, config.osc.targetPort);
 
-let template = JSON.parse(fs.readFileSync(config.template.path).toString());
-let lastTemplateArray = template.texts[template.texts.length - 1].text;
 
-//Initialize
-const osc_client = new osc.Client(config.osc.targetAddress, config.osc.targetPort)
+if(template.texts.length < config.template.page){
+	for(let i = template.texts.length; i < config.template.page; i++){
+		template.texts.push({"text": ["", "", "", "", "", "", "", "", ""]});
+	}
+}
 
-lastTemplateArray.unshift(message);
-template.texts[template.texts.length - 1].text = lastTemplateArray.slice(0, 9)
+let targetTemplateArray = template.texts[config.template.page - 1].text;
+
+targetTemplateArray.unshift(message);
+template.texts[config.template.page - 1].text = targetTemplateArray.slice(0, 9);
 
 fs.writeFileSync(config.template.path, JSON.stringify(template, null, '\t'));
-osc_client.send(config.osc.address.reload, 'reload')
-
-process.stdout.write(iconvLite.encode(message, 'SJIS'));
-
-setTimeout(() => {
-	process.exit(0);
-}, 400)
+osc_client.send(config.osc.address.reload, 'reload', () => {
+	process.stdout.write(iconvLite.encode(message, 'SJIS'));
+	osc_client.close();
+});
 
